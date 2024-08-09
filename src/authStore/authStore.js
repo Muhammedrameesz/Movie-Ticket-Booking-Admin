c7c0d933eda4880501dd925690a18f8b11d3613a
Muhammedrameesz <rameezta555@gmail.com>
2024-08-09 16:19:24 +0530

    final

--- src/authStore/authStore.js
+++ src/authStore/authStore.js
@@ -7,7 +7,7 @@ import axios from "axios";
 const useAuthStore = create((set) => ({
   isAuth: false,
   loading: true,
-  
+
   login: async () => {
     try {
       await verifyAdmin();
@@ -32,8 +32,21 @@ const useAuthStore = create((set) => ({
     try {
       console.log("Attempting to remove adminToken cookie...");
       const cookies = new Cookies();
-      cookies.remove('adminToken', { path: '/', domain: 'movie-ticket-booking-server.onrender.com' });
+      cookies.remove("adminToken", {
+        path: "/",
+        domain: "movie-ticket-booking-server.onrender.com",
+      });
       console.log("Cookie removal attempted.");
+      document.cookie.split(";").forEach((c) => {
+        document.cookie = c
+          .replace(/^ +/, "")
+          .replace(
+            /=.*/,
+            "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;domain=movie-ticket-booking-server.onrender.com"
+          );
+      });
+      console.log("Cookie removal attempted again.");
+
       set({ isAuth: false });
       const event = new Event("logout");
       window.dispatchEvent(event);
@@ -41,9 +54,6 @@ const useAuthStore = create((set) => ({
       console.error("Logout failed:", error);
     }
   },
-  
-  
-  
 
   checkAuth: async () => {
     try {
